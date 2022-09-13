1c1143bdd9744da18190649a6b8defaa60f1a5dd
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-09-13 23:47:57 +0300

    made filter func

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -6,13 +6,17 @@ import { remove } from "redux/store";
 
 const ContactList = () => {
   const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter)
   const dispatch = useDispatch();
   if (!contacts) {
     return;
   }
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
     return (
       <List>
-        {contacts.map(contact => (
+        {filteredContacts.map(contact => (
           <Item key={contact.id}>
             <p>{contact.name}: {contact.number}</p>
             <button onClick={()=>dispatch(remove(contact.id))}>Delete</button>
